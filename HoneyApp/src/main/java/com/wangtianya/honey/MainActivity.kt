fb7f9f0f00fe116d47c2d3f9978a3f94870f1db1
wangtianya <i@wangtianya.cn>
2018-04-14 04:07:44 +0800

    首页初步设计

--- HoneyApp/src/main/java/com/wangtianya/honey/MainActivity.kt
+++ HoneyApp/src/main/java/com/wangtianya/honey/MainActivity.kt
@@ -1,29 +1,57 @@
 package com.wangtianya.honey
 
+import android.app.Activity
 import android.databinding.DataBindingUtil
-import android.databinding.DataBindingUtil.setContentView
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toolbar
+import android.support.v4.widget.NestedScrollView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import com.wangtianya.honey.databinding.ActivityMainBinding
-import com.wangtianya.honey.util.initScreenColor
+import com.wangtianya.honey.util.ScreenManager
 import com.wangtianya.yaa.core.activity.YaaActivity
+import com.wangtianya.yaa.core.util.ScreenUtil
+import com.wangtianya.yaa.core.util.YaaToast
+import java.util.*
+import javax.xml.transform.OutputKeys.METHOD
 
 
 class MainActivity : YaaActivity() {
 
     lateinit var binding: ActivityMainBinding
+    var firstLayout: Boolean = true
 
+
+    @SuppressWarnings(METHOD)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initScreenColor(window, resources.getColor(R.color.colorPrimaryDark), Color.TRANSPARENT)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        initView()
 
+        ScreenManager.initScreenColor(window, resources.getColor(R.color.colorPrimaryDark), Color.TRANSPARENT)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initSwipeRefreshLayout()
+        initView()
     }
 
     fun initView() {
         setActionBar(binding.toolbar)
+
+    }
+
+    fun initSwipeRefreshLayout() {
+        val dp200 = ScreenUtil.dip2px(200, this);
+        binding.swipeRefreshView.setProgressViewOffset(true, 0, dp200)
+        binding.swipeRefreshView.setProgressBackgroundColorSchemeResource(android.R.color.white);
+        // 设置下拉进度的主题颜色
+        binding.swipeRefreshView.setColorSchemeResources(R.color.colorAccent, R.color.colorPrimary, R.color.colorPrimaryDark);
+
+        // 下拉时触发SwipeRefreshLayout的下拉动画，动画完毕之后就会回调这个方法
+        binding.swipeRefreshView.setOnRefreshListener {
+            binding.swipeRefreshView.postDelayed({
+                YaaToast.show("love")
+            }, 1500)
+        }
     }
 
 
--- HoneyApp/src/main/java/com/wangtianya/honey/util/ScreenUtil.kt
+++ HoneyApp/src/main/java/com/wangtianya/honey/util/ScreenUtil.kt
@@ -5,16 +5,22 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 
-fun initScreenColor(window: Window, statusBarColor: Int, navigationBarColor: Int) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        val window = window
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+class ScreenManager {
 
-        // 该参数指布局能延伸到navigationbar，我们场景中不应加这个参数
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.statusBarColor = statusBarColor
-        window.navigationBarColor = navigationBarColor
+    companion object {
+        fun initScreenColor(window: Window, statusBarColor: Int, navigationBarColor: Int) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = statusBarColor
+                window.navigationBarColor = navigationBarColor
+            }
+        }
     }
+
+
 }
