3025489a56841fb27867700e2864c2b67376cb6a
wangtianya <i@wangtianya.cn>
2018-09-13 17:39:28 +0800

    项目拆分

---
+++ Practice/src/main/java/com/wangtianya/practice/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.wangtianya.pratice
+
+import com.qjuzi.yaa.context.YaaApplication
+
+class MyApplication : YaaApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // do something
+    }
+
+}
+
