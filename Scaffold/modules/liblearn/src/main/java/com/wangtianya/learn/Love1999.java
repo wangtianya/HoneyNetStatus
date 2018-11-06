766a35593670bacac1e75c32ba5e9ee86999f3df
wangtianya <i@wangtianya.cn>
2018-11-06 17:56:38 +0800

    update

--- Scaffold/modules/liblearn/src/main/java/com/wangtianya/learn/Love1999.java
+++
@@ -1,9 +0,0 @@
-package com.wangtianya.learn;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Love1999 {
-
-}
